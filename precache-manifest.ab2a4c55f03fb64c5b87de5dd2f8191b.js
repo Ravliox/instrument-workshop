61851a64e9909232b151b3d3bcc06b68cfbe08d6
Ravliox <istratee@gmail.com>
2020-06-19 18:10:45 +0300

    Updates

---
+++ precache-manifest.ab2a4c55f03fb64c5b87de5dd2f8191b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0690fdad61ef207c64d3bf9830d8ec",
+    "url": "/instrument-workshop/index.html"
+  },
+  {
+    "revision": "00d5eea60ad706048f76",
+    "url": "/instrument-workshop/static/css/main.a050f881.chunk.css"
+  },
+  {
+    "revision": "fcc1840e941de5ba5d4e",
+    "url": "/instrument-workshop/static/js/2.ca56a3a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instrument-workshop/static/js/2.ca56a3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d5eea60ad706048f76",
+    "url": "/instrument-workshop/static/js/main.ea331408.chunk.js"
+  },
+  {
+    "revision": "e88535771e20de58f004",
+    "url": "/instrument-workshop/static/js/runtime-main.3ecf23da.js"
+  },
+  {
+    "revision": "ba756bf70903a586883461ddb52f9266",
+    "url": "/instrument-workshop/static/media/cello.ba756bf7.png"
+  },
+  {
+    "revision": "7196ad47551b066f9bae37432169c2f2",
+    "url": "/instrument-workshop/static/media/flute.7196ad47.png"
+  },
+  {
+    "revision": "1ce310fe78a493adebe0ec70c247e498",
+    "url": "/instrument-workshop/static/media/guitar.1ce310fe.png"
+  },
+  {
+    "revision": "2405fbe9c61b8d75cf3710847ff20eb4",
+    "url": "/instrument-workshop/static/media/guitarb.2405fbe9.png"
+  },
+  {
+    "revision": "c754b4740cdc686f138ae4fd8868a32a",
+    "url": "/instrument-workshop/static/media/piano.c754b474.png"
+  },
+  {
+    "revision": "5a22b2eab4310114264dbcec0503f901",
+    "url": "/instrument-workshop/static/media/vblack.5a22b2ea.png"
+  },
+  {
+    "revision": "32688dcf7bffc2f8a9045ffc4b8ab363",
+    "url": "/instrument-workshop/static/media/vmaro.32688dcf.png"
+  }
+]);
