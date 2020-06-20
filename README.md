a6c71cb72be9d753b2428f1098a9dc7bc980c17f
eduard.istrate1 <eduard.istrate@eestec.ro>
2020-06-20 09:45:30 +0300

    added readme

--- README.md
+++ README.md
@@ -1,68 +1,36 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Instrument Workshop
 
-## Available Scripts
+# Eduard Istrate(development) and Bianca Ciobăniță(Web Design) 
 
-In the project directory, you can run:
+This project is a demo for a fictional instrument workshop bussiness. The main purpose of it was to implement a complex style of design 
+initially created in Adobe XD by a designer. The project was built using React.js and is a simple SPA app. 
 
-### `npm start`
+# Structure of the project
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The project is structured in pages and components
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Pages:
+* Home
 
-### `npm test`
+Components: 
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+* Rotating Slider
+* Navbar
+* Expanded Component
 
-### `npm run build`
+While the project only implements a single page of this fictional store the intent was for the structure of the code to reflect how I would 
+structure a project such as this.
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+The **rotating slider** is the central piece of this project, as sliding between the instruments causes various changes to occur in the app such as
+color changes and text changes. The rotating slider is a personal implementation.
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+# Skills showcased
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+The purpose of this project was to showcase the following skills:
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+* Creatings web apps using React.js and knowledge its principles such as: components, lifecycle hooks, state usage and lifting state up and others 
+* programming in Javascript and using OOP principles
+* implementing complex layouts from Adobe XD in to code 
+* CSS animations and transitions 
+* creating responsive web apps (while not all devices were covered due to time the app is mobile and desktop friendly)
+* basic geometry 
--- src/components/RotatingSlider/RotatingSlider.js
+++ src/components/RotatingSlider/RotatingSlider.js
@@ -19,13 +19,14 @@ export default class RotatingSlider extends React.Component {
     constructor(props) {
         super(props);
 
+        /* the images on the slider shall be places on an ellipsis. their translation paramaters will be calculated from the angle */
+        /* on mouse up the images shall move back to a set of angle checkpoints */
         this.state = {
             movement: false,
             resetting: false,
             startX: 0,
             dragReferencePointX: 0,
             radius: (window.innerWidth) / 2 + 400,
-            // radius: 400,
             angleCheckpoints: [18, 90, 162, 234, 306],
             points: [
             {
@@ -100,12 +101,7 @@ export default class RotatingSlider extends React.Component {
         })
     }
 
-    switchDirection(value) {
-        this.setState({
-            direction: value
-        })
-    }
-
+    /* on mouse down saves the X position of the mouse click for future calculations */
     startDrag(event) {
         event.preventDefault();
         event.persist();
@@ -118,15 +114,17 @@ export default class RotatingSlider extends React.Component {
         }
     }
 
+
+    /* on mouse move calculates the angle difference between the initial position and the one the mouse is currently at */
     computeAngles(event) {
         event.persist();
         if(this.state.movement && !this.state.resetting){
             let direction = event.clientX > this.state.dragReferencePointX ? -1 : 1;
-            let difference = Math.abs(event.clientX - this.state.dragReferencePointX) / 20;
+            let difference = Math.abs(event.clientX - this.state.dragReferencePointX) / 20;  // the division is necessary to slow down the movement
             let points = [...this.state.points];
 
             points.forEach(point => {
-                point.angle += direction * difference;
+                point.angle += direction * difference;  //increment the angle proportionally to the difference 
                 if(point.angle > 360) {
                     point.angle = point.angle - 360;
                 } else if (point.angle < 0) {
@@ -142,7 +140,18 @@ export default class RotatingSlider extends React.Component {
         }
     }
 
-    
+
+    /* after angle is computed calculate the x, y coeficients of the point according to the ellipsis formula */
+    setPosition(point) {
+        let angleRadians = (point.angle / 180) * Math.PI;
+        let xCoeficient = Math.round(this.state.radius * Math.cos(angleRadians));
+        let yCoeficient = Math.round(this.state.radius * Math.sin(angleRadians) / 2) * -1;
+
+        point.transform = `translateX(${xCoeficient}px) translateY(${yCoeficient}px)`;
+    }
+
+
+    /* on mouse up stop the movements and calculate the new checkpoints and rotate towards them */
     endDrag(event) {
         if(this.state.movement) {
             event.persist();
@@ -160,6 +169,11 @@ export default class RotatingSlider extends React.Component {
         }
     }
 
+    /* On mouse up set up an interval, the speed of which determines the travel time to the checkpoints. Each tick does the following: 
+    1. determines the point that should be at the central checkpoint
+    2. calculates the delta for a smooth deccelaration using the coordinates of the central point
+    3. increments the angle according to the delta and sets a new transform for the point
+    4. if the point reached the checkpoint interval is cleared */
     moveToCheckpoint() {
         let points = [...this.state.points];
         
@@ -172,6 +186,7 @@ export default class RotatingSlider extends React.Component {
                 let checkPointAngle = this.state.angleCheckpoints[point.checkPoint];
                 let direction = point.angle < checkPointAngle ? 1 : -1;
                 
+                /* necessary checks for angles that leave the 0-360 range. angles must be kept in that range for delta/direction calculations */
                 if(point.checkPoint === 0 && point.angle < 360 && point.angle > 300) {
                     point.angle = point.angle - 358;
                 }
@@ -179,6 +194,8 @@ export default class RotatingSlider extends React.Component {
                 if(point.checkPoint === 4 && point.angle > 0 && point.angle < 72) {
                     point.angle = 358 + point.angle;
                 }
+
+                /* when delta is very low snap the point in position to avoid movement being locked with no visible movement */
                 if(delta === 0) {
                     point.angle = checkPointAngle;
                 }
@@ -203,20 +220,22 @@ export default class RotatingSlider extends React.Component {
                     points
                 })
             }
-        }, 10);
+        }, 8);
     }
 
     calculateDelta(point, checkPointAngle) {
         let t = point.angle < checkPointAngle ? point.angle / checkPointAngle * 2 : checkPointAngle / point.angle * 2;
         let delta = t * (2 - t);
-        return delta > 0.001 && delta < 1 ? delta : 0;
+        return delta > 0.001 && delta < 1 ? delta : 0;      // in case of very low values to avoid prolonged movement and very slow deccelaration set delta to 0
     }
-    
+
+    /* calculates the new checkpoints for the image based on direction and how much the points traveled */
     setNewCheckpoints(event) {
         let points = [...this.state.points];
         let direction = event.clientX > this.state.startX ? 1 : -1;
         let difference = Math.abs(event.clientX - this.state.startX);
 
+        /* arbitrary value to determine wether the points move to next checkpoints or return to their original spots */
         if (difference > 150) {
             points.forEach(point => {
                 let indexOfLeftCheckpoint = Math.floor(point.angle / 72);
@@ -235,14 +254,7 @@ export default class RotatingSlider extends React.Component {
         }   
     }
 
-    setPosition(point) {
-        let angleRadians = (point.angle / 180) * Math.PI;
-        let xCoeficient = Math.round(this.state.radius * Math.cos(angleRadians));
-        let yCoeficient = Math.round(this.state.radius * Math.sin(angleRadians) / 2) * -1;
-
-        point.transform = `translateX(${xCoeficient}px) translateY(${yCoeficient}px)`;
-    }
-
+    /* these two functions are used for rotations of the slider through swiping and the arrow buttons */
     slideRight() {
         if(!this.state.resetting && !this.props.displayTrigger) {
             let points = [...this.state.points];
@@ -269,12 +281,15 @@ export default class RotatingSlider extends React.Component {
         }
     }
 
+    /* calls prop function passed by parent. Used to change the color theme and texts when slider is rotated */
     signalInstrumentChange() {
         let currentInstrument = this.state.points.find(point => point.checkPoint === 1);
         let currentIndex = this.state.points.indexOf(currentInstrument);
         this.props.slideHandler(currentIndex, currentInstrument.img);
     }
 
+
+    /* used for the expanded view of the instrument. Not all instruments have alt images but those that do can flip between them */
     flipAltImage(value) {
         let points = [...this.state.points];
         let currentPoint = points.find(point => point.checkPoint === 1);
